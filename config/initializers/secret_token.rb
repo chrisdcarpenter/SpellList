cf499cf7696ba6fa434e8629841d75e9e79aab3a
chris-carpenter <cdcarpenter@gmail.com>
2012-06-25 16:42:28 -0500

    Post rails new
    Code generated by rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpellList::Application.config.secret_token = '904f3e870ea06dc2dc445f72926a21e5b03f0d010a818a61b2d905d0364726ac3224761f56bd1fe6aed7266c6d727f27c1c2032ac52add40ef5aca390a396aa8'
